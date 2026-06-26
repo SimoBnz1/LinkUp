f1e7bdf21dce90da6ef0cd1a0ab2a879915b26d1
mohamed_bnz <simobnz81@gmail.com>
2026-06-26 12:17:31 +0100

    ajouter le code du seeder pour teste a fake data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         
+         // User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        // Post::factory(8)->create();
+        \App\Models\Post::factory(8)->create();
        
     }
 }
