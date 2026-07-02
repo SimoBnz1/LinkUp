04d36c7289eb99d836f454060dd99801d03bb8d4
mohamed_bnz <simobnz81@gmail.com>
2026-07-02 09:46:46 +0100

    ajouter les deux fonction qui retourn view de register et login dans authController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\RegisterUserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function showLogin()
+    {
+        return view('auth.login');
+    }
+
+    public function showRegister()
+    {
+        return view('auth.register');
+    }
+    
+
+}
