261038f75dc3ee09619d4bca3dd0d818236c10f5
mohamed_bnz <simobnz81@gmail.com>
2026-07-02 09:52:23 +0100

    ajouter la fonction qui return la formulair pour ajouter un post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,17 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class PostController extends Controller
 {
-    public function index()
-{
+      use AuthorizesRequests;
+    public function feed()
+    {
+        $posts = Post::orderBy('created_at', 'desc')->get();
+        return view('feed', compact('posts'));
+    }
+
+  
+
+    public function formPost()
+    {
+        return view('posts.createPost');
+    }
+
+
    
-    $posts = Post::orderBy('created_at', 'desc')->get();
-    return view('feed', compact('posts')); 
-    
-   
-}
 }
